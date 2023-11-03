1bb2ad75e1a13658d1cb4d39976ebb9759f852c2
Alex <alexgjohn@outlook.com>
2023-11-03 15:59:51 +0000

    added word bank file to draw answers from

--- src/Words.js
+++ src/Words.js
@@ -6,3 +6,8 @@ export const boardDefault = [
     ["", "", "", "", ""],
     ["", "", "", "", ""]
 ];
+
+//a set is a data structure like an array, but you can't repeat values and there are no indices
+const generateWordSet = () => {
+    const newSet = new Set()
+}
